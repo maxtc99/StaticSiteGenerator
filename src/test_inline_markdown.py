031e0ad6d55e8ce40cf56fb69739daeb78948ba0
maxtc99 <max.tc99@gmail.com>
2026-03-27 18:38:45 +0000

    added test cases for inline_markdown.py

---
+++ src/test_inline_markdown.py
@@ -0,0 +1,143 @@
+import unittest
+
+from inline_markdown import split_nodes_delimiter, extract_markdown_links, extract_markdown_images
+
+from textnode import TextNode, TextType
+
+
+class TestInlineMarkdown(unittest.TestCase):
+    def test_delim_bold(self):
+        node = TextNode("This is text with a **bolded** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" word", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_bold_double(self):
+        node = TextNode(
+            "This is text with a **bolded** word and **another**", TextType.TEXT
+        )
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" word and ", TextType.TEXT),
+                TextNode("another", TextType.BOLD),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_bold_multiword(self):
+        node = TextNode(
+            "This is text with a **bolded word** and **another**", TextType.TEXT
+        )
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bolded word", TextType.BOLD),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("another", TextType.BOLD),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_italic(self):
+        node = TextNode("This is text with an _italic_ word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_bold_and_italic(self):
+        node = TextNode("**bold** and _italic_", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+        self.assertListEqual(
+            [
+                TextNode("bold", TextType.BOLD),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an image ![python logo](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual(
+            [
+                ("python logo", "https://i.imgur.com/zjjcJKZ.png")
+            ],
+            matches,
+        )
+    
+    def test_extract_markdown_images_2(self):
+        matches = extract_markdown_images(
+            '''
+            This is text with two images ![python logo](https://i.imgur.com/zjjcJKZ.png)
+            and ![python logo 2](https://www.python.org/static/community_logos/python-powered-w-200x80.png)
+            '''
+        )
+        self.assertListEqual(
+            [
+                ("python logo", "https://i.imgur.com/zjjcJKZ.png"),
+                ("python logo 2" ,"https://www.python.org/static/community_logos/python-powered-w-200x80.png")
+            ], 
+            matches,
+        )
+    
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is a text with a link [to google](https://www.google.com)"
+        )
+        self.assertListEqual(
+            [
+                ("to google", "https://www.google.com")
+            ],
+            matches,
+        )
+    
+    def test_extract_markdown_links_2(self):
+        matches = extract_markdown_links(
+            '''
+            This is a text with two links [to google](https://www.google.com)
+            and [to youtube](https://www.youtube.com)
+            '''
+        )
+        self.assertListEqual(
+            [
+                ("to google", "https://www.google.com"),
+                ("to youtube" ,"https://www.youtube.com")
+            ], 
+            matches,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
