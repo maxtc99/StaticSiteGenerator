abdc1b4aa1c15e59a24f851b9c31574c8e2e7c24
maxtc99 <max.tc99@gmail.com>
2026-06-19 19:12:07 +0100

    added HTMLNode class and LeafNode subclass

---
+++ src/htmlnode.py
@@ -0,0 +1,38 @@
+
+
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self) -> str:
+        raise NotImplementedError("to_html method not implemented")
+    
+    def props_to_html(self) -> str:
+        if self.props is None:
+            return ''
+        
+        props_html = ''
+        for key,val in self.props.items():
+            props_html += f' {key}="{val}"'
+        return props_html
+        
+    
+    def __repr__(self) -> str:
+        return f'HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})'
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError("All leaf nodes must have a value")
+        if self.tag is None:
+            return self.value
+        
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+    def __repr__(self) -> str:
+        return f'LeafNode({self.tag}, {self.value}, {self.props})'
