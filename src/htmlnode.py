5ef3da47c12ed3d44d056701b1fca33fe68c29ea
maxtc99 <max.tc99@gmail.com>
2026-06-21 00:38:49 +0100

    added TextNode to HTMLNode function

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -36,3 +36,4 @@ class LeafNode(HTMLNode):
         return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
     def __repr__(self) -> str:
         return f'LeafNode({self.tag}, {self.value}, {self.props})'
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -63,6 +63,7 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_to_html_no_tag(self):
         node = LeafNode(None, "Hello, world!")
         self.assertEqual(node.to_html(), "Hello, world!")
+        
 
 
 if __name__ == "__main__":
