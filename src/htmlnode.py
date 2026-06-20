118bb9dfac15ffc03fee8f80355f5120848cb88d
maxtc99 <max.tc99@gmail.com>
2026-06-21 00:36:59 +0100

    Revert "added TextNode to HTMLNode function"
    This reverts commit 2c1564c915a5f734d77b7aaa33884a0fc1215727.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -27,31 +27,12 @@ class HTMLNode:
 class LeafNode(HTMLNode):
     def __init__(self, tag=None, value=None, props=None):
         super().__init__(tag, value, None, props)
-
     def to_html(self) -> str:
         if self.value is None:
             raise ValueError("All leaf nodes must have a value")
         if self.tag is None:
-            return self.value       
+            return self.value
+        
         return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
-    
     def __repr__(self) -> str:
         return f'LeafNode({self.tag}, {self.value}, {self.props})'
-
-class ParentNode(HTMLNode):
-    def __init__(self, tag=None, children=None, props=None):
-        super().__init__(tag, None, children, props)
-
-    def to_html(self) -> str:
-        if self.tag is None:
-            raise ValueError("tag is missing")
-        if self.children is None:
-            raise ValueError("children node(s) is missing")
-        children_nodes = ''
-        for child in self.children:
-            temp = child.to_html()
-            children_nodes += temp
-        return f"<{self.tag}{self.props_to_html()}>{children_nodes}</{self.tag}>"
-
-    def __repr__(self) -> str:
-        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -49,7 +49,6 @@ class TestHTMLNode(unittest.TestCase):
             "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
         )
 
-    # Test cases for LeafNode
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
@@ -65,50 +64,6 @@ class TestHTMLNode(unittest.TestCase):
         node = LeafNode(None, "Hello, world!")
         self.assertEqual(node.to_html(), "Hello, world!")
 
-    # Test cases for ParentNode
-    def test_to_html_with_children(self):
-        child_node = LeafNode("span", "child")
-        parent_node = ParentNode("div", [child_node])
-        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
-
-    def test_to_html_with_grandchildren(self):
-        grandchild_node = LeafNode("b", "grandchild")
-        child_node = ParentNode("span", [grandchild_node])
-        parent_node = ParentNode("div", [child_node])
-        self.assertEqual(
-            parent_node.to_html(),
-            "<div><span><b>grandchild</b></span></div>",
-        )
-
-    def test_to_html_many_children(self):
-        node = ParentNode(
-            "p",
-            [
-                LeafNode("b", "Bold text"),
-                LeafNode(None, "Normal text"),
-                LeafNode("i", "italic text"),
-                LeafNode(None, "Normal text"),
-            ],
-        )
-        self.assertEqual(
-            node.to_html(),
-            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
-        )
-
-    def test_headings(self):
-        node = ParentNode(
-            "h2",
-            [
-                LeafNode("b", "Bold text"),
-                LeafNode(None, "Normal text"),
-                LeafNode("i", "italic text"),
-                LeafNode(None, "Normal text"),
-            ],
-        )
-        self.assertEqual(
-            node.to_html(),
-            "<h2><b>Bold text</b>Normal text<i>italic text</i>Normal text</h2>",
-        )
 
 if __name__ == "__main__":
     unittest.main()
